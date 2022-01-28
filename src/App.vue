aefbfb43fb37a603c307a985e9bfff140ca7fa58
itcast <727261722@qq.com>
2022-01-28 18:53:00 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide v-if="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide'
+export default {
+  components: {FooterGuide}
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
